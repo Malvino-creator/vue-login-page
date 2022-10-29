262132595f08342ba68aa05c38b0d7034a939085
Moringa School <malvin.opondo@student.moringaschool.com>
2022-10-29 12:26:56 +0300

    Debugging

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,22 @@
       <v-toolbar-title> Vue Login Application</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn text rounded> Home </v-btn>
-      <v-btn text rounded> About</v-btn>
-      <v-btn text rounded> Login</v-btn>
-      <v-btn text rounded> Contact Us</v-btn>
+      <!-- <v-btn text rounded> About</v-btn> -->
+      <!-- <v-btn text rounded> Login</v-btn> -->
+      <!-- <v-btn text rounded> Contact Us</v-btn> -->
     </v-app-bar>
     <v-content>
       <v-card width="600" class="mx auto" mt="6">
         <v-card-title> Log In Area </v-card-title>
         <v-card-text>
           <v-text-field label="Username" prepend-icon="mdi-account-circle">
-            <v-text-field label="Password" type="password" prepend-icon="mdi-lock"
-            append ico>
-            
+            <v-text-field
+              label="Password"
+              type="password"
+              prepend-icon="mdi-lock"
+              append
+              ico
+            >
             </v-text-field>
           </v-text-field>
         </v-card-text>
@@ -30,9 +34,8 @@
 
 <script>
 export default {
-  data()
-   {  
-  return}
-
-}
+  data() {
+    return;
+  },
+};
 </script>
